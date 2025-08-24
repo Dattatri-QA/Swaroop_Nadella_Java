f0ca5633b8af0052924aa868e0f0749e42e1634e
Dattatri <dattatri8077@gmail.com>
2025-08-24 16:31:36 +0530

    this is secound commit...

--- Swaroop_Nadella/src/Day_03/DataType2.java
+++ Swaroop_Nadella/src/Day_03/DataType2.java
@@ -4,11 +4,11 @@ public class DataType2 {
 
 	public static void main(String[] args) {
 		int a;
-		a=10;
-		System.out.println(a);
+		a = 10;
+		System.out.println("this is A" + a);
 		int b;
-		b=20;
-		System.out.println(b);
+		b = 20;
+		System.out.println("this is B" + b);
 
 	}
 
